e7c26e856b4ee99c595581ca76e574c37f404ce2
PRIYANKA SHARMA <115890597+Priyanka101193Sharma@users.noreply.github.com>
2025-04-16 16:46:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,16 +3,16 @@ region = "us-east-1"
 }
 
 resource "aws_instance" "one" {
-count = 5
+count = 4
 ami = "ami-0ddc798b3f1a5117e"
 instance_type = "t2.medium"
-key_name = "swikp"
-vpc_security_group_ids = ["sg-05f044979e305302e"]
+key_name = "netflix"
+vpc_security_group_ids = ["sg-00674eafecb7c8ff4"]
 tags = {
 Name = var.instance_names[count.index]
 }
 }
 
 variable "instance_names" {
-default = ["jenkins", "nexus", "AppServer-1", "AppServer-2", "Monitoring server"]
+default = ["jenkins", "AppServer-1", "AppServer-2", "Monitoring server"]
 }
